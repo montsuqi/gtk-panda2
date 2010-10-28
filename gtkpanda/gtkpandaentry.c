09d9df0cfb0a3a4a3602c2b734313f946f424dea
yusukemihara <mihara@netlab.jp>
2010-10-28 06:44:58 +0000

    * xim_enabledプロパティに変更

--- gtkpanda/gtkpandaentry.c
+++ gtkpanda/gtkpandaentry.c
@@ -151,7 +151,7 @@ gtk_panda_entry_class_init (GtkPandaEntryClass *klass)
   
   g_object_class_install_property (gobject_class,
     PROP_ENABLE_XIM,
-    g_param_spec_boolean ("enable_xim",
+    g_param_spec_boolean ("xim_enabled",
                           _("Enable Input Method"),
                           _("Whether enable input method controll"),
                           FALSE,
