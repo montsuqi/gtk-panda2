7723fe52d4c27980196eac1c7aa5b723c82e272d
yusukemihara <mihara@netlab.jp>
2013-04-17 04:44:58 +0000

    GtkPandaTableでフォーカス移動でコミットされないのを修正

--- gtkpanda/pandacellrenderertext.c
+++ gtkpanda/pandacellrenderertext.c
@@ -131,14 +131,17 @@ editing_done (GtkCellEditable *_entry,
                 "editing-canceled", &canceled,
                 NULL);
   gtk_cell_renderer_stop_editing (GTK_CELL_RENDERER (data), canceled);
+#if 0
   if (canceled) {
     return;
   }
+#endif
 
   path = g_object_get_data (G_OBJECT (_entry), PANDA_CELL_RENDERER_TEXT_PATH);
   new_text = gtk_entry_get_text (GTK_ENTRY (_entry));
 
   if (keyval != 0) {
+    keyval = 0;
     g_signal_emit_by_name (data, "edited-by-key", path, new_text);
   } else {
     g_signal_emit_by_name (data, "edited", path, new_text);
